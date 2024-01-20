45fe3faa8bb38f3396ed3016019be654b315f78d
cultureweb <35487627+cultureweb@users.noreply.github.com>
2024-01-20 12:33:09 +0100

    Update xdebug_state.sh

--- xdebug_state.sh
+++ xdebug_state.sh
@@ -5,10 +5,20 @@ read -p "Enable (1) or disable (0) XDebug ?" XDEBUG_ENABLE
 if [ $XDEBUG_ENABLE -eq 1 ]; then
   echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > /usr/local/etc/php/conf.d/xdebug.ini \
       && echo "xdebug.idekey=PHPSTORM" >> /usr/local/etc/php/conf.d/xdebug.ini \
-      && echo "xdebug.remote_enable=1" >> /usr/local/etc/php/conf.d/xdebug.ini \
-      && echo "xdebug.remote_connect_back=1" >> /usr/local/etc/php/conf.d/xdebug.ini \
-      && echo "xdebug.remote_port=9000" >> /usr/local/etc/php/conf.d/xdebug.ini \
-      && echo "xdebug.remote_log='/tmp/xdebug.log'" >> /usr/local/etc/php/conf.d/xdebug.ini
+      && echo "xdebug.mode=debug" >> /usr/local/etc/php/conf.d/xdebug.ini \
+      && echo "xdebug.discover_client_host=0" >> /usr/local/etc/php/conf.d/xdebug.ini \
+      && echo "xdebug.client_host=$XDEBUG_REMOTE_MACHINE" >> /usr/local/etc/php/conf.d/xdebug.ini \
+      && echo "xdebug.client_port=$XDEBUG_REMOTE_PORT_MACHINE" >> /usr/local/etc/php/conf.d/xdebug.ini \
+      && echo "xdebug.log='/tmp/xdebug.log'" >> /usr/local/etc/php/conf.d/xdebug.ini \
+      && echo "xdebug.log_level=3" >> /usr/local/etc/php/conf.d/xdebug.ini
+
+  read -p "Avec debug en mode CLI (1) ou non (vide) ? : " XDEBUG_REMOTE_AVEC_CLI
+  if [ -z $XDEBUG_REMOTE_AVEC_CLI ]; then
+     unset XDEBUG_CONFIG
+  else
+     export XDEBUG_CONFIG=""
+  fi
+
 else
   echo "" > /usr/local/etc/php/conf.d/xdebug.ini
 fi
